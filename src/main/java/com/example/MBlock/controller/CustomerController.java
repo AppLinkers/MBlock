e236b1320d0c8b56ce5d9b0a666c4dce298cb323
jsw7027 <jsw7027@gmail.com>
2022-04-28 16:50:58 +0900

    announceBoard

--- src/main/java/com/example/MBlock/controller/CustomerController.java
+++ src/main/java/com/example/MBlock/controller/CustomerController.java
@@ -1,17 +1,27 @@
 package com.example.MBlock.controller;
 
+import com.example.MBlock.dto.announced.AnnounceFindRes;
+import com.example.MBlock.service.AnnounceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("customer/")
 @RequiredArgsConstructor
 public class CustomerController {
 
+
+    private final AnnounceService announceService;
+
     @GetMapping("qna")
-    public String getQnaPage() {
+    public String getQnaPage(Model model, @RequestParam(defaultValue = "0") int page) {
+        Page<AnnounceFindRes> announceFindResList = announceService.findAll(page);
+        model.addAttribute("announce", announceFindResList);
         return "customer";
     }
 }

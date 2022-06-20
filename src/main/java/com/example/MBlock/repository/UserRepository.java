ba327e03a100d8545181866f2cca6ac5426116cc
jsw7027 <jsw7027@gmail.com>
2022-06-20 14:48:38 +0900

    login

--- src/main/java/com/example/MBlock/repository/UserRepository.java
+++ src/main/java/com/example/MBlock/repository/UserRepository.java
@@ -10,12 +10,8 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-<<<<<<< HEAD
+
     @Query("select u from User u where u.login_id = :user_id")
     Optional<User> findByUser_id(String user_id);
-=======
-    @Query("select u from User u where u.login_id = :login_id")
-    Optional<User> findByUser_id(String login_id);
->>>>>>> 660345efb489fcb930527d605da9a3a8c648d95f
 
 }

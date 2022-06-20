1f106a5ef193ed7830b5e69dc9f6d1202a29e586
jsw7027 <jsw7027@gmail.com>
2022-06-20 18:01:02 +0900

    signup

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -6,6 +6,20 @@ function defaultBtnActive(){
     defaultBtn.click();
 }
 
+function nullCheck(){
+    const name = document.getElementById("name").value;
+    const id = document.getElementById("id").value;
+    const pw = document.getElementById("password").value;
+    const pwcheck = document.getElementById("password-confirm").value;
+    const position = document.getElementById("name").value;
+    const phone = document.getElementById("name").value;
+    const selectImg = document.getElementById("selectImg").value;
+
+    if(name=="" || id=="" ||pw=="" || pwcheck=="" || position=="" || phone =="" || selectImg==""){
+        alert("빈 칸이 있습니다. 다시 한번 확인해주세요.");
+    }
+}
+
 defaultBtn.addEventListener("change",function(){
     const file = this.files[0];
     if(file){

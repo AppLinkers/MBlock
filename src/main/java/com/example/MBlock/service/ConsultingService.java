1724b819962f1a1f6aa84ce6e56914cfbae69714
yuseogi0218 <yuseogi0218@gmail.com>
2022-06-22 15:06:40 +0900

    t

--- src/main/java/com/example/MBlock/service/ConsultingService.java
+++ src/main/java/com/example/MBlock/service/ConsultingService.java
@@ -1,10 +1,14 @@
 package com.example.MBlock.service;
 
 import com.example.MBlock.domain.Consulting;
+import com.example.MBlock.dto.Consulting.GetConsultingRes;
 import com.example.MBlock.dto.Consulting.WriteConsultingReq;
 import com.example.MBlock.repository.ConsultingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +27,25 @@ public class ConsultingService {
 
         consultingRepository.save(consulting);
 
+    }
+
+    public List<GetConsultingRes> getAllConsulting() {
+        List<GetConsultingRes> result = new ArrayList<>();
+
+        List<Consulting> consultingList = consultingRepository.findAll();
+
+        consultingList.forEach(
+                c -> {
+                    result.add(GetConsultingRes.builder()
+                            .id(c.getId())
+                            .name(c.getName())
+                            .phone(c.getPhone())
+                            .email(c.getEmail())
+                            .context(c.getContext())
+                            .trader(c.getTrader()).build());
+                }
+        );
+
+        return result;
     }
 }

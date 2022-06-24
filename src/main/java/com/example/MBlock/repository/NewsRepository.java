46bf5ba3691c7b3c2221782ed2641865b17ede1c
yuseogi0218 <yuseogi0218@gmail.com>
2022-06-24 13:03:19 +0900

    t

--- src/main/java/com/example/MBlock/repository/NewsRepository.java
+++ src/main/java/com/example/MBlock/repository/NewsRepository.java
@@ -1,9 +1,13 @@
 package com.example.MBlock.repository;
 
 import com.example.MBlock.domain.News;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
+
+    Slice<News> findAllBy(Pageable pageable);
 }

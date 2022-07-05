a23da3468a83b5849fc5645dba07030bf4288545
yuseogi0218 <yuseogi0218@gmail.com>
2022-07-05 19:37:02 +0900

    t

--- src/main/java/com/example/MBlock/advice/FormAuthenticationProvider.java
+++ src/main/java/com/example/MBlock/advice/FormAuthenticationProvider.java
@@ -1,12 +1,15 @@
 package com.example.MBlock.advice;
 
 import com.example.MBlock.domain.UserDetail;
+import com.example.MBlock.domain.type.Approved;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -27,7 +30,11 @@ public class FormAuthenticationProvider implements AuthenticationProvider {
         UserDetail userDetail = (UserDetail) userDetailsService.loadUserByUsername(username);
 
         if (!passwordEncoder.matches(password, userDetail.getPassword())) {
-            throw new BadCredentialsException("비밀번호가 틀립니다.");
+            throw new BadCredentialsException("invalid");
+        }
+
+        if (userDetail.getAuthorities().contains(new SimpleGrantedAuthority(Approved.PENDING.toString()))) {
+            throw new InsufficientAuthenticationException("unauthorized");
         }
 
         return new UsernamePasswordAuthenticationToken(username, password, userDetail.getAuthorities());

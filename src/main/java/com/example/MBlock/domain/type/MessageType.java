54621cd27cec909244ccd49e59e9f899f867e94e
yuseogi0218 <yuseogi0218@gmail.com>
2022-07-13 19:46:40 +0900

    img_chat

--- src/main/java/com/example/MBlock/domain/type/MessageType.java
+++ src/main/java/com/example/MBlock/domain/type/MessageType.java
@@ -4,5 +4,5 @@ public enum MessageType {
     TALK,
     ENTER,
     EXIT,
-    PHOTO
+    FILE
 }
--- src/main/java/com/example/MBlock/repository/ConsultingRepository.java
+++ src/main/java/com/example/MBlock/repository/ConsultingRepository.java
@@ -1,13 +1,10 @@
 package com.example.MBlock.repository;
 
-import com.example.MBlock.domain.Announce;
 import com.example.MBlock.domain.Consulting;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ConsultingRepository extends JpaRepository<Consulting, Long> {

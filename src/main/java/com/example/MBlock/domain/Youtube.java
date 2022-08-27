85f05fe9bf0ca473041dc775d46ffb7da071205f
yuseogi0218 <yuseogi0218@gmail.com>
2022-08-27 09:17:34 +0900

    FEAT - youtube domain

--- src/main/java/com/example/MBlock/domain/Youtube.java
+++ src/main/java/com/example/MBlock/domain/Youtube.java
@@ -3,6 +3,7 @@ package com.example.MBlock.domain;
 import javax.persistence.*;
 
 import com.example.MBlock.domain.listener.Auditable;
+import com.example.MBlock.support.BooleanToYNConverter;
 import lombok.*;
 
 @Entity
@@ -23,6 +24,7 @@ public class Youtube extends BaseEntity implements Auditable {
 
     private String imgUrl;
 
+    @Convert(converter = BooleanToYNConverter.class)
     private boolean onAir;
 
     private String API_KEY;
